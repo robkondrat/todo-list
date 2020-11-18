d1fdc92e35b44cb2081a6755513df039d2d312b9
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-11-18 11:40:51 -0600

    clear completed tasks button

--- src/App.js
+++ src/App.js
@@ -7,10 +7,25 @@ import ToDoList from "./ToDoList";
 
 function App() {
   const [ toDoList, setToDoList ] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id == id ? { ...task, complete: !task.complete } : { ...task };
+    });
+    setToDoList(mapped);
+  }
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList}/>
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter} />
     </div>
   )
 }
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ToDo from './ToDo';
 
-const ToDoList = ({toDoList}) => {
+const ToDoList = ({toDoList, handleToggle, handleFilter }) => {
   return (
     <div>
       {toDoList.map(toDo => {
         return (
-          <ToDo toDo={toDo} />
+          <ToDo toDo={toDo} handleToggle={handleToggle} handleFilter={handleFilter} />
         )
       })}
+      <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
     </div>
   )
 }
