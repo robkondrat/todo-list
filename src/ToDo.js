7b0ea9988dcb1ef452a48260546ccdf141b7e376
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-11-18 11:57:32 -0600

    add tasks, tutorial done

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const ToDo = ({toDo}) => {
+const ToDo = ({todo, handleToggle}) => {
+  const handleClick = (e) => {
+    e.preventDefault()
+    handleToggle(e.currentTarget.id)
+  }
+
   return (
     <div>
-      <div className={toDo.complete ? "strike" : ""}>
-        {toDo.task}
+      <div id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} onClick={handleClick} className={todo.complete ? "todo strike" : "todo"}>
+        {todo.task}
       </div>
     </div>
   )
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -4,9 +4,9 @@ import ToDo from './ToDo';
 const ToDoList = ({toDoList, handleToggle, handleFilter }) => {
   return (
     <div>
-      {toDoList.map(toDo => {
+      {toDoList.map(todo => {
         return (
-          <ToDo toDo={toDo} handleToggle={handleToggle} handleFilter={handleFilter} />
+          <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter} />
         )
       })}
       <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
